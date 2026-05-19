e42db17ed11d170b8d30c10075a8c258284e0480
chouchane.louai20@gmail.com <chouchane.louai20@gmail.com>
2026-05-19 18:40:13 +0100

    feat: Insert One Post

--- handlers.go
+++ handlers.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type BlogPost struct {
+	PostId string	`bson:"_id"`
+	Title string	`bson:"title"`
+	Techs string	`bson:"techs"`
+	Status string	`bson:"status"`
+	Clickme string	`bson:"clickme"`
+	Tryme string	`bson:"tryme,omitempty"`
+	Readme string	`bson:"readme,omitempty"`
+}
+
+func CreatePost(client *mongo.Client, dbName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		coll := client.Database(dbName).Collection("posts")
+
+		title := r.FormValue("title")
+		techs := r.FormValue("techs")
+		status := r.FormValue("status")
+		clickme := r.FormValue("clickme")
+		tryme := r.FormValue("tryme")
+		readme := r.FormValue("readme")
+
+		blogPost := &BlogPost{
+			Title: title,
+			Techs: techs,
+			Status: status,
+			Clickme: clickme,
+			Tryme: tryme,
+			Readme: readme,
+		}
+
+		result, err := coll.InsertOne(context.TODO(), blogPost)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Document Inserted with success, _id:%v", result.InsertedID)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+
+		if err := json.NewEncoder(w).Encode(result); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
@@ -37,11 +40,34 @@ func connectToDatabase(dbName string) *MongoClient{
 
 func main(){
 	godotenv.Load()
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+    	// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+    	AllowedOrigins:   []string{"https://*", "http://*"},
+    	// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+  }))
 	dabataseName := os.Getenv("DATABASE_NAME")
 	mongoClient = *connectToDatabase(dabataseName)
-	defer func(){
-		if err := mongoClient.Client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
+	r.Route("/blogs", func(r chi.Router){
+		r.Post("/create", CreatePost(mongoClient.Client, dabataseName))
+	})
+	
+	var port string = fmt.Sprintf(":%s", os.Getenv("PORT"))
+	fmt.Println(port)
+	http.ListenAndServe(port, r)
+
+	// defer func(){
+	// 	if err := mongoClient.Client.Disconnect(context.TODO()); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+
+
 }
